9522356a51ad1f9dee61a0765f8bdf06ab7b3eb9
paluszkiewiczB <49494671+paluszkiewiczB@users.noreply.github.com>
2021-03-18 21:38:21 +0100

    fix bash

--- .github/bumpTag.sh
+++ .github/bumpTag.sh
@@ -2,9 +2,13 @@
 set -eu
 
 lastTag=$(git tag -l | tail -n 1)
+echo "lastTag: $lastTag"
 version=${lastTag##*-}
 major=${version%.*}
 minor=${version#*.}
+echo "major: $major, minor: $minor"
 nextMinor=$((minor+1))
+
+echo "next version: $major.$nextMinor"
 
 git tag "$major.$nextMinor"
